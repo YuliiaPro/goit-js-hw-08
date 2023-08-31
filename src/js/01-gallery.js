db69434f3eea93a1c2ce2cae57a2c61b6df75bde
YuliiaPro <yuliiaeducation@gmail.com>
2023-08-31 22:54:11 +0300

    first and second task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const listEl = document.querySelector(".gallery");
+listEl.style.listStyle = 'none';
+
+const galleryList = (arr, container) => {
+    const galleryMarkup = arr.
+    map(({ preview, original, description }) => 
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" title="${description}" src="${preview}" alt="${description}" />
+    </a>
+    </li>`
+    ) 
+    .join("");
+    container.insertAdjacentHTML("beforeend", galleryMarkup);
+ };
+
+galleryList(galleryItems, listEl);
+
+
+const modalInstance = new SimpleLightbox(".gallery a", {captionDelay: 250, captionPosition:"button"});
+console.log(modalInstance);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_TIME_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function(e) {
+    localStorage.setItem(STORAGE_TIME_KEY, e.seconds)
+};
+
+player.on('timeupdate', throttle(onPlay,1000));
+
+player.setCurrentTime(localStorage.getItem(STORAGE_TIME_KEY));
